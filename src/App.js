ac802154ef8c3ea3675eaec08e8819e56ca63e06
aafzal <ambreenfafzal@gmail.com>
2019-08-16 18:03:26 -0500

    dded public folder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Components/Header";
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+<Header />
+    </div>
+  )
+
+
+}
+
+export default App;
---
+++ src/Components/Header/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+     <h1>Don't Click Twice</h1>
+     <h3>Test your memory</h3>
+     <h5>Click on each image once to earn points</h5>
+        </p>
+     
+      </header>
+    </div>
+  );
+}
+
+export default Header;
